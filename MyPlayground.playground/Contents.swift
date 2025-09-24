7677d7d220443109b50f52d63be2279a1ca2fef0
Florian Degraeve <floriandegraeve@DESKTOP-O9FGCEO.vivesnet.be>
2025-09-24 10:46:21 +0200

    extra oefeningen

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -31,3 +31,61 @@ func reverseString (string: String?) -> String? {
 print(reverseString(string: "Vives"))
 
 
+func getUpperLowerCount (string: String) -> (String , String, Int) {
+    return (string.uppercased(),string.lowercased(), string.count)
+}
+var ios = getUpperLowerCount(string: "iOS 26")
+
+print(ios.0)
+print(ios.1)
+print(ios.2)
+print(ios)
+func divide (int1: Int, int2: Int) -> Double {
+    
+    return  Double (int1) / Double (int2)
+}
+divide(int1: 0, int2: 0)
+
+func calculate ( numbers : Double...) -> (Double, Double?, Double?) {
+    var avg = 0.0
+    for number in numbers {
+        avg += number
+        
+    }
+    avg = avg / Double (numbers.count)
+    return (avg, numbers.min(), numbers.max())
+    
+}
+calculate(numbers: 10, 0, 5)
+calculate(numbers: 4,5,6,-3)
+calculate(numbers: -3)
+calculate()
+
+var x = 10.0
+var y = 3
+
+enum StringConversionError : Error {
+    case nilParameter
+    case emtpyParameter
+}
+
+func getUpperLowerCount (string: String?) throws -> (String , String, Int) {
+    if string == nil {
+        throw StringConversionError.nilParameter
+    }
+    if string == "" {
+        throw StringConversionError.emtpyParameter
+    }
+    return (string!.uppercased(),string!.lowercased(), string!.count)
+}
+
+
+struct Iphone {
+    var suplier = "Apple"
+    var type : String
+    var height: Double
+    var width : Double
+    
+}
+let iphone = Iphone(type: "", height: 0.0, width: 0.0)
+
